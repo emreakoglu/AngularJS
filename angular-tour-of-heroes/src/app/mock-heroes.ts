154491c506b922c2f3fe83a50ee65fc5d6671e38
emreakoglu <emre.akoglu@bil.omu.edu.tr>
2018-11-29 09:44:15 -0800

    Input from Another Component

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 11, name: 'Bombasto'},
+    {id: 11, name: 'Celeeritas'},
+    {id: 11, name: 'Magneta'},
+    {id: 11, name: 'RubberMan'},
+    {id: 11, name: 'Dynama'},
+    {id: 11, name: 'Dr IQ'},
+    {id: 11, name: 'Magma'},
+    {id: 11, name: 'Tornado'}
+];
